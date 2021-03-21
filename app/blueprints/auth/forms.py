e90cd4c75d6d7d3f53f9ccf1596ec41dd7526d20
Brianna Swales <briannaswales@Briannas-MacBook-Pro.local>
2021-03-21 16:50:12 -0400

    my contribution

--- app/blueprints/auth/forms.py
+++ app/blueprints/auth/forms.py
@@ -1,26 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import ValidationError, DataRequired, EqualTo, Email
-from app.models import User
+from wtforms.validators import DataRequired, EqualTo, Email
 
 
 class UserInfoForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    # confirm_pass = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    confirm_pass = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField()
-
-    # def validate_username(self, username):
-    #     user = User.query.filter_by(username=username.data).first()
-    #     if user is not None:
-    #         raise ValidationError('Please use a different username.')
-
-    # def validate_email(self, email):
-    #     user = User.query.filter_by(email=email.data).first()
-    #     if user is not None:
-    #         raise ValidationError('Please use a different email address.')
-
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
